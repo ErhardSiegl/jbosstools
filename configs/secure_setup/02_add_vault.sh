8674c139e766b125212eee94577f6b085eb1f804
Erhard Siegl <erhard.siegl@objectbay.com>
2013-04-05 17:08:46 +0200

    Adding vault with CLI

--- configs/secure_setup/02_add_vault.sh
+++ configs/secure_setup/02_add_vault.sh
@@ -5,28 +5,6 @@ echo use: $KEYSTORE_DIR
 mkdir $KEYSTORE_DIR
 cp ENC.dat  Shared.dat vault.keystore $KEYSTORE_DIR
 
-cat<<'EOF'>tmp_vault
-
-    <vault>
-        <vault-option name="KEYSTORE_URL" value="${jboss.server.config.dir}/keystores/vault.keystore"/>
-        <vault-option name="KEYSTORE_PASSWORD" value="MASK-1CjcBL7EGFMlqb/ECsmaHH"/>
-        <vault-option name="KEYSTORE_ALIAS" value="vault"/>
-        <vault-option name="SALT" value="12345678"/>
-        <vault-option name="ITERATION_COUNT" value="33"/>
-        <vault-option name="ENC_FILE_DIR" value="${jboss.server.config.dir}/keystores/"/>
-    </vault>
-
-EOF
-
-ed $JBOSS_HOME/standalone/configuration/standalone.xml <<EOF
-/management
--
-.r tmp_vault
-wq
-EOF
-
-rm tmp_vault
-
 # Keystore generiert mit 
 
 # keytool -genkey -alias vault -keyalg RSA -keysize 1024 -keystore vault.keystore <<EOF

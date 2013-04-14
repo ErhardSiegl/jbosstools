de43a3ca7ccc45c5fcbed04711b78165cf7f45fd
Erhard Siegl <erhard.siegl@objectbay.com>
2013-04-14 15:07:42 +0200

    Use openssl instead of add-user.sh

--- configs/basic_setup/01_create_user.sh
+++ configs/basic_setup/01_create_user.sh
@@ -1,1 +1,11 @@
-$JBOSS_HOME/bin/add-user.sh admin jboss ManagementRealm --silent=true 
+# include(config.m4)dnl
+# $JBOSS_HOME/bin/add-user.sh admin jboss ManagementRealm --silent=true 
+
+USERS_FILE=$JBOSS_HOME/standalone/configuration/mgmt-users.properties
+
+USER="admin"
+PWD="jboss"
+
+# changequote(<!,!>)
+echo $USER=$( echo -n $USER:ManagementRealm:$PWD | openssl dgst -md5 -hex | cut -d" " -f2 ) > $USERS_FILE
+# changequote()

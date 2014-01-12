bc018aa5c50bef980793393eb314135bae7d6a63
Erhard Siegl <erhard.siegl@objectbay.com>
2014-01-12 16:04:36 +0100

    New rules for passwords

--- configs/ticket2rock/01_create_user.sh
+++ configs/ticket2rock/01_create_user.sh
@@ -1,7 +1,7 @@
 RoleFile=$JBOSS_HOME/standalone/configuration/application-roles.properties
 
-$JBOSS_HOME/bin/add-user.sh admin jboss ManagementRealm
-$JBOSS_HOME/bin/add-user.sh -a guest guest1 ApplicationRealm
+$JBOSS_HOME/bin/add-user.sh admin jboss@123 ManagementRealm
+$JBOSS_HOME/bin/add-user.sh -a guest guest@123 ApplicationRealm
 
 sed -i 's/^.*guest=.*/guest=guest/' $RoleFile
 #sed -i 's/^.*guest=/#&/' $RoleFile

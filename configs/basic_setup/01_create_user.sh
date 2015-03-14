14688863683e3ae059fcb1b099ec39be6173262a
Erhard Siegl <erhard.siegl@objectbay.com>
2015-03-14 21:29:50 +0100

    Security configs adapted

--- configs/basic_setup/01_create_user.sh
+++ configs/basic_setup/01_create_user.sh
@@ -5,7 +5,7 @@ USERS_FILE=$JBOSS_HOME/standalone/configuration/mgmt-users.properties
 DOMAIN_USERS_FILE=$JBOSS_HOME/domain/configuration/mgmt-users.properties
 
 USER="admin"
-PWD="jboss"
+PWD="admin@123"
 
 # changequote(<!,!>)
 echo $USER=$( echo -n $USER:ManagementRealm:$PWD | openssl dgst -md5 -hex | cut -d" " -f2 ) > $USERS_FILE

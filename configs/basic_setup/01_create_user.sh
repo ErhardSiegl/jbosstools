cf7ca21e35294fa064775dbe17c8f09df867b700
Herbert Wirnsberger <herbert.wirnsberger@objectbay.com>
2015-04-24 12:02:02 +0200

    create user ausgetauscht

--- configs/basic_setup/01_create_user.sh
+++ configs/basic_setup/01_create_user.sh
@@ -1,14 +1,1 @@
-# include(config.m4)dnl
-# $JBOSS_HOME/bin/add-user.sh admin jboss ManagementRealm --silent=true 
-
-USERS_FILE=$JBOSS_HOME/standalone/configuration/mgmt-users.properties
-DOMAIN_USERS_FILE=$JBOSS_HOME/domain/configuration/mgmt-users.properties
-
-USER="admin"
-PWD="jboss"
-
-# changequote(<!,!>)
-echo $USER=$( echo -n $USER:ManagementRealm:$PWD | openssl dgst -md5 -hex | cut -d" " -f2 ) > $USERS_FILE
-# changequote()
-
-cp $USERS_FILE $DOMAIN_USERS_FILE
+$JBOSS_HOME/bin/add-user.sh admin admin@123 ManagementRealm --silent=true 
